e7c69c20ce5a0a3789c01515738949f103a0d197
Md Lutfor Rahman <61651795+webatozexpert@users.noreply.github.com>
2021-10-10 09:34:10 +0600

    role update
    role update

--- qal/app/Http/Controllers/UserController.php
+++ qal/app/Http/Controllers/UserController.php
@@ -11,40 +11,35 @@ use DB;
 
 class UserController extends Controller
 {
-    
     public function all_user()
     {
         //dd('Ok');
        
         $allusers = DB::table('users')
-                    ->select('users.*','userroles.id as uid','userroles.role_name')
-                    ->leftJoin('userroles','userroles.id','=','users.user_role')
-                    ->get();
+                ->select('users.*','userroles.id as uid','userroles.role_name')
+                ->leftJoin('userroles','userroles.id','=','users.user_role')
+                ->get();
 
         //dd($userrole);
         return view('user/alluser',compact('allusers'));
     }
 
-   
     public function create()
     {
         //dd('ok');
         $userrole = DB::table('userroles')->get();
-        $branch = DB::table('branchs')->get();
+        $branch   = DB::table('branchs')->get();
         return view('user/create',compact('userrole','branch'));
     }
 
-    
     public function store(Request $request)
     {
        //dd('OK');
        // dd($request->all());
-        
-
         $request->validate([
-            'name'       => 'required',
-            'email'      => 'required|unique:users',
-            'password'   => 'required|confirmed|min:6'
+            'name'      => 'required',
+            'email'     => 'required|unique:users',
+            'password'  => 'required|confirmed|min:6'
         ]);
 
         DB::table('users')->insert([
@@ -57,32 +52,27 @@ class UserController extends Controller
             'password'  => Hash::make($request->get('password')),
         ]);
 
-       
        return redirect::to('all-user')->with('success', 'Successfully Added.');
     }
 
-    
-
-    
     public function edit($id)
 
     {
         $edit = DB::table('users')->where('id',$id)->first();
 
         $userrole = DB::table('userroles')->get();
-        $branch = DB::table('branchs')->get();
+        $branch   = DB::table('branchs')->get();
         return view('user/edit',compact('edit','userrole','branch'));
     }
-
-   
+ 
     public function update(Request $request)
     {
         //dd($request->all());
 
         $request->validate([
-            'name'       => 'required',
-            'email'      => 'required',
-            'password'   => 'required|min:6'
+            'name'      => 'required',
+            'email'     => 'required',
+            'password'  => 'required|min:6'
         ]);
 
         DB::table('users')->where('id',$request->get('id'))->update([
@@ -104,15 +94,12 @@ class UserController extends Controller
         return redirect::to('all-user')->with('success', 'Successfully Added.');
     }
 
-   
-
      public function delete($id){
 
         $delete = DB::table('users')
                 ->where('id',$id)
                 ->delete();
         
-       
         return redirect::to('all-user')->with('success', 'Delete Successfully .');
     }
 }
--- qal/resources/views/user/edit.blade.php
+++ qal/resources/views/user/edit.blade.php
@@ -3,7 +3,7 @@
 <div class="content-wrapper">
   <div class="card">
     <div class="card-body">
-      
+
       <div class="row">
         <div class="col-7 table-responsive">
           <h3 class="card-title text-center">User Registration</h3>
@@ -11,7 +11,7 @@
             {{ csrf_field() }}    <!-- token -->
             <input type="hidden" name="id" value="{{ $edit->id }}">
             <div class="form-group row">
-             <label for="zoneCode" class="col-sm-3 col-form-label">User Name</label>
+              <label for="zoneCode" class="col-sm-3 col-form-label">User Name</label>
               <div class="col-sm-9" style="padding-left: 0;">
                 <input type="text" class="form-control" id="name" name="name" placeholder="User Name" autocomplete="off" value="{{ $edit->name }}" >
               </div>
@@ -28,57 +28,56 @@
             @error('email')
             <span class="text-semibold text-danger">{{$message}}</span>
             @enderror
-              <div class="form-group row">
-               <label for="zoneName" class="col-sm-3 col-form-label">User Role</label>
-               <div class="col-sm-9" style="padding-left: 0;">
+            <div class="form-group row">
+              <label for="zoneName" class="col-sm-3 col-form-label">User Role</label>
+              <div class="col-sm-9" style="padding-left: 0;">
                 <select name="user_role" id="user_role" class="select2 form-control custom-select" style="width: 100%;" >
                   <option value="">Select</option>
-                 
+
                   @foreach($userrole as $rows)
                   <option value="{{ $rows->id }}" @if($edit->user_role==$rows->id) selected="" @endif>{{ $rows->role_name }}</option>
                   @endforeach
 
-
                 </select>
 
-               {{--  {{$edit->user_role}} --}}
+                {{--  {{$edit->user_role}} --}}
               </div>
             </div>
             @error('user_role')
             <span class="text-semibold text-danger">{{$message}}</span>
             @enderror
-              <div class="form-group row">
+            <div class="form-group row">
               <label for="zoneName" class="col-sm-3 col-form-label">User Group</label>
-               <div class="col-sm-9" style="padding-left: 0;">
-                    <select name="user_group" id="user_group" class="select2 form-control custom-select" style="width: 100%;" required="">
-                   
-                      <option value="QAL" @if($edit->user_group=='QAL') selected="" @endif>QAL</option>
-                      <option value="QFL" @if($edit->user_group=='QFL') selected="" @endif>QFL</option>
-                      <option value="QIL" @if($edit->user_group=='QIL') selected="" @endif>QIL</option>
-                      <option value="QBL" @if($edit->user_group=='QBL') selected="" @endif>QBL</option>
-                      <option value="ABL" @if($edit->user_group=='ABL') selected="" @endif>ABL</option>
-                     
-                    </select>
+              <div class="col-sm-9" style="padding-left: 0;">
+                <select name="user_group" id="user_group" class="select2 form-control custom-select" style="width: 100%;" required="">
+
+                  <option value="QAL" @if($edit->user_group=='QAL') selected="" @endif>QAL</option>
+                  <option value="QFL" @if($edit->user_group=='QFL') selected="" @endif>QFL</option>
+                  <option value="QIL" @if($edit->user_group=='QIL') selected="" @endif>QIL</option>
+                  <option value="QBL" @if($edit->user_group=='QBL') selected="" @endif>QBL</option>
+                  <option value="ABL" @if($edit->user_group=='ABL') selected="" @endif>ABL</option>
+
+                </select>
               </div>
             </div>
             @error('user_group')
             <span class="text-semibold text-danger">{{$message}}</span>
             @enderror
-             <div class="form-group row">
+            <div class="form-group row">
               <label for="zoneName" class="col-sm-3 col-form-label">User Status</label>
-                <div class="col-sm-9" style="padding-left: 0;">
-                    <select name="status" id="status" class="select2 form-control custom-select" style="width: 100%;" >
-                     <option value="1">Active</option>
-                     <option value="2">Inactive</option>
-                      <option value="1" @if($edit->status=='1') selected="" @endif>Active</option>
-                       <option value="2" @if($edit->status=='2') selected="" @endif>Inactive</option>
-                    </select>
-                </div>
+              <div class="col-sm-9" style="padding-left: 0;">
+                <select name="status" id="status" class="select2 form-control custom-select" style="width: 100%;" >
+                  <option value="1">Active</option>
+                  <option value="2">Inactive</option>
+                  <option value="1" @if($edit->status=='1') selected="" @endif>Active</option>
+                  <option value="2" @if($edit->status=='2') selected="" @endif>Inactive</option>
+                </select>
+              </div>
             </div>
             @error('status')
             <span class="text-semibold text-danger">{{$message}}</span>
             @enderror
-             <div class="form-group row">
+            <div class="form-group row">
               <label for="zoneName" class="col-sm-3 col-form-label">New Password</label>
               <div class="col-sm-9" style="padding-left: 0;">
                 <input type="password" class="form-control" id="password" name="password" placeholder="Enter minimum 6 character password" autocomplete="off"  >
@@ -87,61 +86,59 @@
             @error('password')
             <span class="text-semibold text-danger">{{$message}}</span>
             @enderror
-           {{--  <div class="form-group row">
-              <label for="zoneName" class="col-sm-3 col-form-label">Confirm Password</label>
-              <div class="col-sm-9" style="padding-left: 0;">
-                <input type="password" class="form-control" id="confirm_password" name="confirm_password" placeholder="Enter confirm password" autocomplete="off" >
-              </div>
-            </div>
-            @error('confirm_password')
-            <span class="text-semibold text-danger">{{$message}}</span>
-            @enderror --}}
-            
-       &nbsp;&nbsp;&nbsp;&nbsp;
-		          <div class="row">
-		            <div class="col-12">
-		              <div class="card">
-		                <div class="card-body" style="padding: 0px;">
-		                  <div class="table-responsive">
-		                    <table class="table ">
-		                      <thead>
-		                        <tr>
-		                          <th>User Location</th>
-		                          <th>Mark</th>
-		                          <th>Default</th>
-		                        </tr>
-		                      </thead>
-		                      <tbody>
-		                      @foreach($branch as $results)
-		                        <tr>
-			                        <td>{{ $results->name }}</td>
-			                        <td><input type="checkbox" name="approved_id[]" id="approved_id" value="{{ $results->id }}"></a>
-	                                 </td>
-			                         
-			                         
-			                         <td><input type="radio" name="approved_id[]" id="approved_id" value="{{ $results->id }}"></a>
-	                                 </td>
-		                        </tr>
-		                        @endforeach
-		                      </tbody>
-		                    </table>
-		                  </div>
+{{--  <div class="form-group row">
+<label for="zoneName" class="col-sm-3 col-form-label">Confirm Password</label>
+<div class="col-sm-9" style="padding-left: 0;">
+<input type="password" class="form-control" id="confirm_password" name="confirm_password" placeholder="Enter confirm password" autocomplete="off" >
+</div>
+</div>
+@error('confirm_password')
+<span class="text-semibold text-danger">{{$message}}</span>
+@enderror --}}
 
+&nbsp;&nbsp;&nbsp;&nbsp;
+<div class="row">
+  <div class="col-12">
+    <div class="card">
+      <div class="card-body" style="padding: 0px;">
+        <div class="table-responsive">
+          <table class="table ">
+            <thead>
+              <tr>
+                <th>User Location</th>
+                <th>Mark</th>
+                <th>Default</th>
+              </tr>
+            </thead>
+            <tbody>
+              @foreach($branch as $results)
+              <tr>
+                <td>{{ $results->name }}</td>
+                <td>
+                  <input type="checkbox" name="approved_id[]" id="approved_id" value="{{ $results->id }}"></a> 
+                </td>
 
-		                </div>
-		              </div>
-		            </div>
-		          </div>
-		         <div class="form-group row text-right">
-                <div class="col-sm-12">
-                <button type="submit" class="btn btn-success mr-2">Submit</button>
-              </div>
-            </div>
-          </form>
+                <td><input type="radio" name="approved_id[]" id="approved_id" value="{{ $results->id }}"></a>
+                </td>
+              </tr>
+              @endforeach
+            </tbody>
+          </table>
         </div>
       </div>
     </div>
   </div>
+</div>
+<div class="form-group row text-right">
+  <div class="col-sm-12">
+    <button type="submit" class="btn btn-success mr-2">Submit</button>
+  </div>
+</div>
+</form>
+</div>
+</div>
+</div>
+</div>
 </div>
 
 @endsection
